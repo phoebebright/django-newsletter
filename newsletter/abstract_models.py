dd75c4db35f8d93706116da1c49f8bc57e270da6
phoebebright <phoebebright310@gmail.com>
2025-08-25 15:00:01 +0100

    auto generate slug for message if not supplied

--- newsletter/abstract_models.py
+++ newsletter/abstract_models.py
@@ -11,6 +11,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.db import models
 from django.template.loader import select_template
 from django.utils.functional import cached_property
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import gettext
 from django.utils.timezone import now
